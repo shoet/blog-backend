b01d31665308ccac0714a5bb0bc534760679d476
shoet <32211937+shoet@users.noreply.github.com>
2023-10-09 22:33:19 +0900

    interface作成

---
+++ backend/interfaces/repositories.go
@@ -0,0 +1,23 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/shoet/blog/models"
+)
+
+type BlogRepository interface {
+	Add(ctx context.Context, db *sqlx.DB, blog *models.Blog) (models.BlogId, error)
+	List(ctx context.Context, db *sqlx.DB, option ListBlogOptions) ([]*models.Blog, error)
+	Delete(ctx context.Context, db *sqlx.DB, id models.BlogId) error
+	Put(ctx context.Context, db *sqlx.DB, blog *models.Blog) error
+}
+
+type UserRepository interface {
+	Add(ctx context.Context, db *sqlx.DB, user *models.User) (models.UserId, error)
+	Get(ctx context.Context, db *sqlx.DB, id models.UserId) (*models.User, error)
+	GetByEmail(ctx context.Context, db *sqlx.DB, email string) (*models.User, error)
+	Delete(ctx context.Context, db *sqlx.DB, id models.UserId) error
+	Put(ctx context.Context, db *sqlx.DB, user *models.User) error
+}
