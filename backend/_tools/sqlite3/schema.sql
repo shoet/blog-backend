4914ece49e2c14d490f582e538051aee60013646
shoet <32211937+shoet@users.noreply.github.com>
2023-10-09 11:31:14 +0900

    DB追加

---
+++ backend/_tools/sqlite3/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `blogs` (
+  `id`          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  `author_id`   INTEGER NOT NULL,
+  `title`       TEXT NOT NULL,
+  `content`     TEXT NOT NULL,
+  `thumbnail_image_file_name` TEXT,
+  `is_public`   BOOLEAN NOT NULL DEFAULT 1,
+  `created`     DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `modified`    DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE `tags` (
+  `id`          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  `name`        TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE blogs_tags (
+  id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  blog_id     INTEGER NOT NULL,
+  tag_id      INTEGER NOT NULL,
+  FOREIGN KEY(blog_id) REFERENCES blogs(id) ON DELETE CASCADE,
+  FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE,
+  UNIQUE(blog_id, tag_id)
+);
--- backend/go.mod
+++ backend/go.mod
@@ -4,5 +4,6 @@ go 1.19
 
 require (
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/jmoiron/sqlx v1.3.5
 	golang.org/x/sync v0.4.0
 )
