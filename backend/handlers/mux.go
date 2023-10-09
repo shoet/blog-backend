81011a2ce2860bdb53aeca548c56067ef9c3467f
shoet <32211937+shoet@users.noreply.github.com>
2023-10-09 09:44:05 +0900

    api基礎追加

---
+++ backend/handlers/mux.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi/v5"
+)
+
+func NewMux() *chi.Mux {
+	router := chi.NewRouter()
+	router.Route("/", func(r chi.Router) {
+		r.Use(CORSMiddleWare)
+		r.Route("/health", func(r chi.Router) {
+			hh := &HealthCheckHandler{}
+			r.Get("/", hh.ServeHTTP)
+		})
+	})
+	return router
+}
