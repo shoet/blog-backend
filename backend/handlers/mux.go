2acac73b1a53644e5882a99ff0955829a5fa6ed5
shoet <32211937+shoet@users.noreply.github.com>
2023-12-02 18:25:41 +0900

    修正

--- backend/handlers/mux.go
+++ backend/handlers/mux.go
@@ -29,89 +29,92 @@ func NewMux(
 	router := chi.NewRouter()
 	authMiddleWare := NewAuthorizationMiddleware(deps.JWTer)
 	corsMiddleWare := NewCORSMiddleWare(deps.Config)
-	router.Use(logging.WithLoggerMiddleware(*deps.Logger))
+	router.Use(logging.WithLoggerMiddleware(*deps.Logger), corsMiddleWare)
 
-	router.Route("/", func(r chi.Router) {
-		r.Use(corsMiddleWare)
-		r.Route("/health", func(r chi.Router) {
-			hh := &HealthCheckHandler{}
-			r.Get("/", hh.ServeHTTP)
-		})
+	router.Route("/health", func(r chi.Router) {
+		hh := &HealthCheckHandler{}
+		r.Get("/", hh.ServeHTTP)
+	})
 
-		r.Route("/blogs", func(r chi.Router) {
-			blh := &BlogListHandler{
-				Service: deps.BlogService,
-			}
-			r.Get("/", blh.ServeHTTP)
+	router.Route("/blogs", func(r chi.Router) {
+		blh := &BlogListHandler{
+			Service: deps.BlogService,
+		}
+		r.Get("/", blh.ServeHTTP)
 
-			bgh := &BlogGetHandler{
-				Service: deps.BlogService,
-				jwter:   deps.JWTer,
-			}
-			r.Get("/{id}", bgh.ServeHTTP)
+		bgh := &BlogGetHandler{
+			Service: deps.BlogService,
+			jwter:   deps.JWTer,
+		}
+		r.Get("/{id}", bgh.ServeHTTP)
 
-			// require login
-			bah := &BlogAddHandler{
-				Service:   deps.BlogService,
-				Validator: deps.Validator,
-			}
-			r.With(authMiddleWare.Middleware).Post("/", bah.ServeHTTP)
+		// require login
+		bah := &BlogAddHandler{
+			Service:   deps.BlogService,
+			Validator: deps.Validator,
+		}
+		r.With(authMiddleWare.Middleware).Post("/", bah.ServeHTTP)
 
-			// require login
-			bdh := &BlogDeleteHandler{
-				Service:   deps.BlogService,
-				Validator: deps.Validator,
-			}
-			r.With(authMiddleWare.Middleware).Delete("/", bdh.ServeHTTP)
+		// require login
+		bdh := &BlogDeleteHandler{
+			Service:   deps.BlogService,
+			Validator: deps.Validator,
+		}
+		r.With(authMiddleWare.Middleware).Delete("/", bdh.ServeHTTP)
 
-			// require login
-			buh := &BlogPutHandler{
-				Service:   deps.BlogService,
-				Validator: deps.Validator,
-			}
-			r.With(authMiddleWare.Middleware).Put("/", buh.ServeHTTP)
-		})
+		// require login
+		buh := &BlogPutHandler{
+			Service:   deps.BlogService,
+			Validator: deps.Validator,
+		}
+		r.With(authMiddleWare.Middleware).Put("/", buh.ServeHTTP)
+	})
 
-		r.Route("/tags", func(r chi.Router) {
-			th := &TagListHandler{
-				Service: deps.BlogService,
-			}
-			r.Get("/", th.ServeHTTP)
-		})
+	router.Route("/tags", func(r chi.Router) {
+		th := &TagListHandler{
+			Service: deps.BlogService,
+		}
+		r.Get("/", th.ServeHTTP)
+	})
 
-		r.Route("/files", func(r chi.Router) {
-			// require login
-			gt := GenerateThumbnailImageSignedURLHandler{
-				StorageService: deps.StorageService,
-				Validator:      deps.Validator,
-			}
-			r.With(authMiddleWare.Middleware).Post("/thumbnail/new", gt.ServeHTTP)
+	router.Route("/files", func(r chi.Router) {
+		// require login
+		gt := GenerateThumbnailImageSignedURLHandler{
+			StorageService: deps.StorageService,
+			Validator:      deps.Validator,
+		}
+		r.With(authMiddleWare.Middleware).Post("/thumbnail/new", gt.ServeHTTP)
 
-			gc := GenerateContentsImageSignedURLHandler{
-				StorageService: deps.StorageService,
-				Validator:      deps.Validator,
-			}
-			r.With(authMiddleWare.Middleware).Post("/content/new", gc.ServeHTTP)
-		})
+		gc := GenerateContentsImageSignedURLHandler{
+			StorageService: deps.StorageService,
+			Validator:      deps.Validator,
+		}
+		r.With(authMiddleWare.Middleware).Post("/content/new", gc.ServeHTTP)
+	})
 
-		r.Route("/auth", func(r chi.Router) {
-			ah := NewAuthLoginHandler(deps.AuthService, deps.Validator, deps.Config)
-			r.Post("/signin", ah.ServeHTTP)
+	router.Route("/auth", func(r chi.Router) {
+		ah := NewAuthLoginHandler(deps.AuthService, deps.Validator, deps.Config)
+		r.Post("/signin", ah.ServeHTTP)
 
-			ash := NewAuthSessionLoginHandler(deps.AuthService, deps.Config)
-			r.Get("/login/me", ash.ServeHTTP)
+		ash := NewAuthSessionLoginHandler(deps.AuthService, deps.Config)
+		r.Get("/login/me", ash.ServeHTTP)
 
-			alh := NewAuthLogoutHandler(deps.Config)
-			r.Post("/admin/signout", alh.ServeHTTP)
-		})
+		alh := NewAuthLogoutHandler(deps.Config)
+		r.Post("/admin/signout", alh.ServeHTTP)
+	})
 
-		r.Route("/admin", func(r chi.Router) {
-			bla := &BlogListAdminHandler{
-				Service: deps.BlogService,
-			}
-			r.With(authMiddleWare.Middleware).Get("/blogs", bla.ServeHTTP)
-		})
-
+	router.Route("/admin", func(r chi.Router) {
+		bla := &BlogListAdminHandler{
+			Service: deps.BlogService,
+		}
+		r.With(authMiddleWare.Middleware).Get("/blogs", bla.ServeHTTP)
 	})
 	return router, nil
+}
+
+func setHealthRoute(r chi.Router) {
+	r.Route("/health", func(r chi.Router) {
+		hh := &HealthCheckHandler{}
+		r.Get("/", hh.ServeHTTP)
+	})
 }
