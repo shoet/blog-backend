55a6e0a9d9547060c71747dc0b8079ed3eea5a74
shoet <32211937+shoet@users.noreply.github.com>
2023-12-02 10:18:05 +0900

    テスト追加

--- backend/util/bcrypt.go
+++ backend/util/bcrypt.go
@@ -6,7 +6,12 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+var ErrPasswordEmpty = fmt.Errorf("password is empty")
+
 func HashPassword(password string) (string, error) {
+	if password == "" {
+		return "", ErrPasswordEmpty
+	}
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 	if err != nil {
 		return "", fmt.Errorf("failed hash password")
