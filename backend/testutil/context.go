7558cbe0a7e68e196fae912b311b35fc11b2c1e9
shoet <32211937+shoet@users.noreply.github.com>
2023-12-05 18:46:49 +0900

    修正

--- backend/testutil/context.go
+++ backend/testutil/context.go
@@ -2,8 +2,9 @@ package testutil
 
 import (
 	"context"
-	"io"
+	// "io"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/shoet/blog/logging"
@@ -11,6 +12,7 @@ import (
 
 func SetLoggerContextToRequest(t *testing.T, request *http.Request) *http.Request {
 	t.Helper()
-	logger := logging.NewLogger(io.Discard) // テスト時はロガーによるログ出力を捨てる
-	return request.WithContext(context.WithValue(request.Context(), logging.LoggerKey, logger))
+	// logger := logging.NewLogger(io.Discard) // テスト時はロガーによるログ出力を捨てる
+	logger := logging.NewLogger(os.Stdout)
+	return request.WithContext(context.WithValue(request.Context(), logging.LoggerKey{}, logger))
 }
