e1ad2aac187013eb426239d8a79014c2da5ab912
shoet <32211937+shoet@users.noreply.github.com>
2024-03-09 11:43:46 +0900

    PostgreSQL向けDB構築

---
+++ _tools/migrations/postgres/20240309105217-mod-updated_at-auto-update.sql
@@ -0,0 +1,28 @@
+
+-- +migrate Up
+
+-- +migrate StatementBegin
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.modified = EXTRACT(EPOCH FROM CURRENT_TIMESTAMP);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_blogs_trigger_mod
+BEFORE UPDATE ON blogs
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+CREATE TRIGGER update_users_trigger_mod
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+-- +migrate StatementEnd
+
+-- +migrate Down
+DROP TRIGGER update_blogs_trigger_mod;
+DROP TRIGGER update_users_trigger_mod;
+DROP FUNCTION update_updated_at_column;
