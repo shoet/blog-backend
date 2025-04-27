ec34e0510a64ee81c5933e7b4fcdd0ec2ce4e5b5
shoet <32211937+shoet@users.noreply.github.com>
2025-04-27 12:14:44 +0900

    オプショナルにはomitemptyを適用する

--- internal/infrastructure/models/user_profile.go
+++ internal/infrastructure/models/user_profile.go
@@ -9,8 +9,8 @@ type UserProfile struct {
 	UserId              UserId        `json:"userId" db:"user_id"`
 	Nickname            string        `json:"nickname" db:"nickname"`
 	AvatarImageFileName *string       `json:"avatarImageFileName,omitempty" db:"avatar_image_file_name"`
-	AvatarImageFileURL  *string       `json:"avatarImageFileURL"`
-	Biography           *string       `json:"bio" db:"bio"`
+	AvatarImageFileURL  *string       `json:"avatarImageFileURL,omitempty"`
+	Biography           *string       `json:"bio,omitempty" db:"bio"`
 	Created             time.Time     `json:"created" db:"created"`
 	Modified            time.Time     `json:"modified" db:"modified"`
 }
